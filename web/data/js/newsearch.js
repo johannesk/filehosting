50c2d3317f32566f32c4fcae13036767562c423e
johannes <johannes@phlox.wohnen.chaos-disciple.org>
2009-09-22 18:44:48 +0200

    web: the search box is automaticly selecton on load

---
+++ web/data/js/newsearch.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+	$("div.search_box input[name=tags]").focus()
+});
---
+++ web/header/js/newsearch.js
@@ -0,0 +1,4 @@
+Content-Length: 81
+Content-Type: application/x-javascript
+Last-Modified: Tue, 22 Sep 2009 16:41:24 GMT
+
